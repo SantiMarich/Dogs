ea0317c96ec499b7006db252f1b0b06c0124f2c5
SantiMarich <santiagomarich@gmail.com>
2023-05-28 01:55:42 -0300

    BackEndOK

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import { Route } from "react-router-dom";
+import { Home, Landing, Form, Detail } from "./views/Views";
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/create" component={Form} />
+      <Route exact path="/detail/:id" component={Detail} />
     </div>
   );
 }
---
+++ client/src/components/Components.jsx
@@ -0,0 +1,12 @@
+import Card from "./Body/Card/Card";
+import Filter from "./Body/Filter/Filter";
+import Paginate from "./Body/Paginate/Paginate";
+import Container from "./Body/Container";
+
+import Footer from "./Footer/Footer";
+
+import Header from "./Header/Header";
+import SearchBar from "./Header/SearchBar/SearchBar";
+import NavBar from "./Header/NavBar/NavBar";
+
+export { Card, Filter, Paginate, Container, Footer, Header, SearchBar, NavBar };
